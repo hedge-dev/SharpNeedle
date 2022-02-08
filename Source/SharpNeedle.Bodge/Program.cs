c7a6dc98f290350ff2bf2f926166fce2523648ad
Sajidur78 <sajidur78@gmail.com>
2022-02-09 01:13:32 +0600

    Implement ShadowModel from LW

--- Source/SharpNeedle.Bodge/Program.cs
+++ Source/SharpNeedle.Bodge/Program.cs
@@ -1,5 +1,10 @@
 ﻿// Program for quickly writing temporary things
-using SharpNeedle.HedgehogEngine.Mirage;
-var info = ResourceUtility.Open<GITextureGroupInfo>(@"D:\Unpacked CPK\Sonic Generations\bb\Packed\ghz200\ghz200\gi-texture.gi-texture-group-info");
-info.Write("gi-texture.gi-texture-group-info");
-// Console.Read();
+using SharpNeedle.LostWorld;
+using SharpNeedle.LostWorld.Animation;
+
+//var res = new BinaryResource<BinaryPrimitive<int>>();
+//res.Read(FileSystem.Open(@"D:\Unpacked CPK\Sonic Lost World\sonic2013_0\set\batl01_obj_00.orc"));
+
+var model = ResourceUtility.Open<ShadowModel>(@"D:\Games\SteamLibrary\steamapps\common\Sonic Lost World\mods\Link Sonic\disk\sonic2013_patch_0\Sonic\chr_Sonic.shadow-model");
+model.Write(FileSystem.Create("chr_Sonic.shadow-model"));
+Console.Read();
