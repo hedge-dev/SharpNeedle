921cbfedfe07a1c9f4363de5a9e8dc786f273c7f
Justin113D <Justin113D@gmail.com>
2024-11-15 19:40:11 +0100

    Added Needle texture streaming implementation (reading & writing) Based on code provided by @blueskythlikesclouds

---
+++ Source/SharpNeedle/HedgehogEngine/Needle/TextureStreaming/DataBlock.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+
+namespace SharpNeedle.HedgehogEngine.Needle.TextureStreaming;
+
+public class DataBlock : IBinarySerializable
+{
+    public long Position { get; set; }
+
+    public long Length { get; set; }
+
+    public MemoryStream DataStream { get; private set; }
+
+
+    public void Read(BinaryObjectReader reader)
+    {
+        Position = reader.ReadInt64();
+        Length = reader.ReadInt64();
+    }
+
+    public void Write(BinaryObjectWriter writer)
+    {
+        if(DataStream == null)
+        {
+            throw new InvalidOperationException("Data block has no buffered data!");
+        }
+
+        writer.WriteOffset(WriteData, 1);
+        writer.WriteInt64(DataStream.Length);
+    }
+
+    private void WriteData(BinaryObjectWriter writer)
+    {
+        long previousPosition = DataStream.Position;
+        DataStream.Seek(0, SeekOrigin.Begin);
+        writer.WriteBytes(DataStream.ReadAllBytes());
+        DataStream.Seek(previousPosition, SeekOrigin.Begin);
+    }
+
+
+    public void EnsureData(Package package, bool overrideExisting)
+    {
+        if(DataStream != null)
+        {
+            if(overrideExisting)
+            {
+                DataStream.Dispose();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        DataStream = new MemoryStream((int)Length);
+
+        if(package != null)
+        {
+            using SubStream readStream = new(package.BaseStream, Position, Length);
+            DataStream.Write(readStream.ReadAllBytes().AsSpan());
+        }
+
+        Position = 0;
+    }
+}
---
+++ Source/SharpNeedle/HedgehogEngine/Needle/TextureStreaming/Info.cs
@@ -0,0 +1,96 @@
+﻿using System.IO;
+
+namespace SharpNeedle.HedgehogEngine.Needle.TextureStreaming;
+
+public class Info : ResourceBase, IBinarySerializable
+{
+    public string PackageName { get; set; }
+    public int Mip4x4Index { get; set; }
+    public byte[] Mip4x4 { get; set; }
+    public byte[] DdsHeader { get; set; }
+
+    public override void Read(IFile file)
+    {
+        Name = file.Name;
+        BaseFile = file;
+
+        using Stream stream = file.Open();
+        using BinaryObjectReader reader = new(stream, StreamOwnership.Transfer, Endianness.Little);
+        Read(reader);
+    }
+
+    public override void Write(IFile file)
+    {
+        using Stream stream = file.Open(FileAccess.Write);
+        BinaryObjectWriter writer = new(stream, StreamOwnership.Transfer, Endianness.Little, Encoding.ASCII);
+        Write(writer);
+    }
+
+    public void Read(BinaryObjectReader reader)
+    {
+        if(reader.ReadInt32() != 0x4953544E)
+        {
+            throw new InvalidDataException("Invalid signature, expected NTSI");
+        }
+
+        if(reader.ReadInt32() != 1)
+        {
+            throw new InvalidDataException("Invalid version, expected 1");
+        }
+
+        if(reader.ReadInt32() != 0)
+        {
+            throw new InvalidDataException("Invalid reserved value, expected 0");
+        }
+
+        int nameSize = reader.ReadInt32();
+        int mip4x4Size = reader.ReadInt32();
+        Mip4x4Index = reader.ReadInt32();
+        PackageName = reader.ReadString(StringBinaryFormat.FixedLength, nameSize);
+        Mip4x4 = reader.ReadArray<byte>(mip4x4Size);
+        DdsHeader = reader.ReadArray<byte>((int)(reader.Length - reader.Position));
+    }
+
+    public void Write(BinaryObjectWriter writer)
+    {
+        writer.WriteInt32(0x4953544E);
+        writer.WriteInt32(1);
+        writer.WriteInt32(0);
+
+        writer.WriteInt32(PackageName.Length);
+        writer.WriteInt32(Mip4x4.Length);
+        writer.WriteInt32(Mip4x4Index);
+
+        writer.WriteString(StringBinaryFormat.NullTerminated, PackageName);
+        writer.WriteBytes(Mip4x4);
+        writer.WriteBytes(DdsHeader);
+    }
+
+    
+    public byte[] UnpackDDS(Package package)
+    {
+        string entryName = Path.GetFileNameWithoutExtension(Name);
+        if(!package.TryFind(entryName, out Package.EntryInfo entry))
+        {
+            throw new InvalidDataException($"Package \"{package.Name}\" has no entry for info \"{entryName}\"");
+        }
+
+        MemoryStream result = new();
+
+        result.Write(DdsHeader);
+
+        for(int i = 0; i < entry.MipLevels; i++)
+        {
+            DataBlock block = package.Blocks[entry.BlockIndex + i];
+
+            Stream stream = block.DataStream ?? package.BaseStream;
+
+            stream.Seek(block.Position, SeekOrigin.Begin);
+            result.Write(stream.ReadBytes((int)block.Length));
+        }
+
+        result.Flush();
+
+        return result.ToArray();
+    }
+}
