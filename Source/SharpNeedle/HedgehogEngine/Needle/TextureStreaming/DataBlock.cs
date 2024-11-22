7fee87ffe541ece13f85ffbc1b81854e3a28e4fe
Justin113D <41839896+Justin113D@users.noreply.github.com>
2024-11-22 09:02:23 +0100

    Implemented reading & writing of `Needle texture streaming` files (#22)

---
+++ Source/SharpNeedle/HedgehogEngine/Needle/TextureStreaming/DataBlock.cs
@@ -0,0 +1,48 @@
+﻿namespace SharpNeedle.HedgehogEngine.Needle.TextureStreaming;
+
+using System.IO;
+
+public class DataBlock : IBinarySerializable
+{
+    public Stream DataStream { get; set; }
+
+
+    public void Read(BinaryObjectReader reader)
+    {
+        long position = reader.ReadInt64();
+        long length = reader.ReadInt64();
+        DataStream = new SubStream(reader.GetBaseStream(), position, length);
+    }
+
+    public void Write(BinaryObjectWriter writer)
+    {
+        if(DataStream == null)
+        {
+            throw new InvalidOperationException("Data block has no buffered data!");
+        }
+
+        writer.WriteOffset(WriteData, 1);
+        writer.WriteInt64(DataStream.Length);
+    }
+
+    private void WriteData(BinaryObjectWriter writer)
+    {
+        long previousPosition = DataStream.Position;
+        DataStream.Seek(0, SeekOrigin.Begin);
+        writer.WriteBytes(DataStream.ReadAllBytes());
+        DataStream.Seek(previousPosition, SeekOrigin.Begin);
+    }
+
+
+    public void EnsureData()
+    {
+        if(DataStream == null)
+        {
+            return;
+        }
+
+        byte[] data = DataStream.ReadAllBytes();
+        DataStream.Dispose();
+        DataStream = new MemoryStream(data);
+    }
+}
