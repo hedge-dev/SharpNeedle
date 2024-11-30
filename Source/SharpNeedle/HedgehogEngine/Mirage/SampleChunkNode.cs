f8900282dc67f618cb323a1f76de1cf268d92d1f
Justin113D <41839896+Justin113D@users.noreply.github.com>
2024-11-30 21:16:14 +0100

    Removed accidentally added using-declaration

--- Source/SharpNeedle/HedgehogEngine/Mirage/SampleChunkNode.cs
+++ Source/SharpNeedle/HedgehogEngine/Mirage/SampleChunkNode.cs
@@ -2,7 +2,6 @@
 
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Reflection.Metadata.Ecma335;
 
 public class SampleChunkNode : IBinarySerializable, IEnumerable<SampleChunkNode>
 {
