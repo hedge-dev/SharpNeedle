e30bc3754eba348b53255e35a159d61722e02a49
Justin113D <Justin113D@gmail.com>
2024-12-01 11:53:17 +0100

    Reset bodge

--- Source/SharpNeedle.Bodge/Program.cs
+++ Source/SharpNeedle.Bodge/Program.cs
@@ -1,12 +1,1 @@
 ﻿// Program for quickly writing temporary things
-using SharpNeedle.HedgehogEngine.Mirage;
-
-var parameters = new ShaderParameter();
-parameters.Read(@"SurfRide3DTransform.vsparam");
-parameters.AddFloat4Usage("g_Screen_Size", 0xF4);
-parameters.Save();
-
-foreach (var usage in parameters.AllUsages())
-{
-    Console.WriteLine($"{usage} : {usage.Type.ToString().ToLower()}");
-}
