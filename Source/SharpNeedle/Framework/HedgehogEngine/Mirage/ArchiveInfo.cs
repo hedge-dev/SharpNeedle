e0f523e8c56d4fd0eddcb529a98d7176b962dab0
BlueCube3310 <53150244+BlueCube3310@users.noreply.github.com>
2025-03-12 17:07:39 +0100

    Add support for .arcinfo files (#40)

---
+++ Source/SharpNeedle/Framework/HedgehogEngine/Mirage/ArchiveInfo.cs
@@ -0,0 +1,58 @@
+﻿namespace SharpNeedle.Framework.HedgehogEngine.Mirage;
+
+[NeedleResource("hh/arcinfo", @"\.arcinfo$")]
+public class ArchiveInfo : SampleChunkResource
+{
+    public List<Entry> Entries { get; set; } = [];
+
+    public ArchiveInfo()
+    {
+        DataVersion = 1;
+    }
+
+    public override void Read(BinaryObjectReader reader)
+    {
+        int entryCount = reader.Read<int>();
+        Entries.AddRange(reader.ReadObjectArrayOffset<Entry>(entryCount));
+
+        reader.ReadOffset(() =>
+        {
+            for(int i=0; i< entryCount; i++)
+            {
+                Entries[i].Field04 = reader.Read<byte>();
+            }
+        });
+    }
+
+    public override void Write(BinaryObjectWriter writer)
+    {
+        writer.Write(Entries.Count);
+        writer.WriteObjectCollectionOffset(Entries);
+
+        writer.WriteOffset(() =>
+        {
+            for (int i = 0; i < Entries.Count; i++)
+            {
+                writer.Write(Entries[i].Field04);
+            }
+
+            writer.Align(4);
+        });
+    }
+
+    public class Entry : IBinarySerializable
+    {
+        public string? Name { get; set; }
+        public byte Field04 { get; set; } // Priority?
+
+        public void Read(BinaryObjectReader reader)
+        {
+            Name = reader.ReadStringOffset();
+        }
+
+        public void Write(BinaryObjectWriter writer)
+        {
+            writer.WriteStringOffset(StringBinaryFormat.NullTerminated, Name, -1, 1);
+        }
+    }
+}
